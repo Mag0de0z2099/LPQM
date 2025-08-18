6ffb95f17501eb6c15633197ea0a9e5e4df0d637
Mag0de0z2099 <107160320+Mag0de0z2099@users.noreply.github.com>
2025-08-18 09:35:17 -0600

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+project = "LPQM Simulator"
+author = "Tu Nombre"
+release = "0.1"
+
+extensions = ["sphinx.ext.autodoc"]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+html_theme = "sphinx_rtd_theme"
